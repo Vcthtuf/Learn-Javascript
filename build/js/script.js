f25be9ee96b821c4ff706ab6169f38bc3cfd6591
Vcthtuf <Vcthtuf@gmail.com>
2021-02-04 21:47:09 +0500

    Added funtions in script.js

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,8 @@
 'use strict'
 
-let money = +prompt('Ваш бюджет на месяц?', ''),
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money, time;
+
+start();
 
 let appData = {
     budget: money,
@@ -15,25 +16,47 @@ let appData = {
 console.log(appData.budget);
 console.log(appData.timeData);
 
+chooseExpenses();
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Введите обязательную статью расходов', '');
-    let b = +prompt('Во сколько обойдется?', '');
-
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
-        console.log('done');
-        appData.expenses[a] = b;
-    } else {
-        i--;
-    }
-};
-
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed(2);
 
 alert('Ваш бюджет на день ' + appData.moneyPerDay);
 
 console.log(appData);
 
+checkSavings();
+
+function start() {
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
+    }
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+}
+
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Введите обязательную статью расходов', '');
+        let b = +prompt('Во сколько обойдется?', '');
+
+        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+            console.log('done');
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        }
+    };
+}
+
+function checkSavings() {
+    appData.savings = confirm('Вы имеете накопления?');
+    if (appData.savings) {
+        let save = +prompt('Какова сумма Ваших накоплений?', ''),
+            persent = +prompt('Под какой процент?', '');
+
+        appData.monthIncome = (save * persent / 1200).toFixed(2);
+        alert('Ваша прибыль в месяц: ' + appData.monthIncome);
+    }
+}
 
 
 
