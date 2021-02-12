90a135ad789a279a6280f66014a7fef395a79a1e
Vcthtuf <Vcthtuf@gmail.com>
2021-02-12 22:58:35 +0500

    Output messages, used for-in and forEach

--- build/js/script.js
+++ build/js/script.js
@@ -61,6 +61,13 @@ let appData = {
     }
 };
 
+
+console.log(`Наша программа включает в себя данные:`);
+for (let key in appData) {
+    console.log(`${key}: ${appData[key]}`);
+}
+
+
 console.log(appData);
 
 
